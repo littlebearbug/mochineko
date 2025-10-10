34290ffce0864d73f139842e68366dd4d2df72f2
vibeBill <bill.zhu@vibe.us>
2025-10-10 16:31:38 +0800

    feat: 启用github pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? '/mochineko' : '',
+  assetPrefix: isProd ? '/mochineko/' : '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
