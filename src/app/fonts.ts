dc4cfe11ab55d40333c2eb7f3f34de4f740e0ce5
vibeBill <bill.zhu@vibe.us>
2025-10-21 14:52:15 +0800

    refactor: 使用next/font重构字体

---
+++ src/app/fonts.ts
@@ -0,0 +1,9 @@
+import { Noto_Sans_SC } from 'next/font/google';
+
+export const noto_sans_sc = Noto_Sans_SC({
+  subsets: ['latin'],
+
+  display: 'swap',
+
+  variable: '--font-noto-sans-sc',
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import Navbar from '@/components/navigation/Navbar';
 import Footer from '@/components/navigation/Footer';
+import { noto_sans_sc } from './fonts';
 
 export const metadata: Metadata = {
   title: {
@@ -16,7 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="zh" data-scroll-behavior="smooth">
+    <html
+      lang="zh"
+      data-scroll-behavior="smooth"
+      className={`${noto_sans_sc.variable}`}
+    >
       <body>
         <Navbar />
         {children}
